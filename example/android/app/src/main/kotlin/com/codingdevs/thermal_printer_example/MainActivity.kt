36484751b5b000eef16371f1f5eef9c77701b7cc
kechankrisna <ke.chankrisna168@gmail.com>
2025-05-24 17:53:45 +0700

    chore: fixed on android

--- example/android/app/src/main/kotlin/com/codingdevs/thermal_printer_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/codingdevs/thermal_printer_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.codingdevs.thermal_printer_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
